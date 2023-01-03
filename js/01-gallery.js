65fc41e166fab9ea2019801b2d62d62bdc4ef5b8
Aleksandr Borysenko <borysenko343@gmail.com>
2023-01-03 16:04:09 +0200

    HW 07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
+const gallaryMarkup = createGalleryItem(galleryItems);
 
-console.log(galleryItems);
+galleryRef.insertAdjacentHTML("afterbegin", gallaryMarkup);
+
+galleryRef.addEventListener('click', onImageClick);
+
+function createGalleryItem(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a>
+        </div>`
+    })
+        .join('');
+}
+
+function onImageClick(event) {
+event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    const value = event.target.dataset.source;
+    const instance = basicLightbox.create(`<img src="${value}" width="800" height="600">`);
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
+const gallaryMarkup = createGalleryItem(galleryItems);
 
-console.log(galleryItems);
+galleryRef.insertAdjacentHTML("afterbegin", gallaryMarkup);
+
+function createGalleryItem(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<a class="gallery__link" href="${original}">
+                    <img class="gallery__image" 
+                    src="${preview}" 
+                    alt="${description}"
+                    "/>
+                        </a>`
+    })
+        .join('');
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
